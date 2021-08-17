8a0e65beaca2c1ff049610ec64b352953e87b3af
lucsimao <lucasimao64@gmail.com>
2021-08-17 02:19:42 -0300

    refactor: adding some feature in axiosClientHelper
      - getTestlinkRPCConfig
      - parseTestlinkUrl

--- src/client/util/axiosClientHelper.ts
+++ src/client/util/axiosClientHelper.ts
@@ -1,11 +1,27 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
+import { TestLink } from 'testlink-xmlrpc';
+
 export class AxiosClientHelper {
+  public static getTestlinkRPCConfig(
+    host: string,
+    port: number,
+    apiKey: string
+  ): TestLink {
+    return new TestLink({
+      host: host,
+      port: port,
+      secure: true,
+      apiKey: apiKey,
+    });
+  }
+
   public static async getClientResponse<T>(
     requestConfig: AxiosRequestConfig,
     routeName: string
   ): Promise<T[]> {
     this.setTestlinkApiKey(requestConfig);
+    this.setTestlinkUrl(requestConfig);
     const response = await this.getAxiosResponse<T>(requestConfig, routeName);
     return this.parseRequestResponse<T>(response);
   }
@@ -31,10 +47,22 @@ export class AxiosClientHelper {
     requestConfig.headers.APIKEY = requestConfig.headers.testlinkApiKey;
   }
 
+  private static setTestlinkUrl(requestConfig: AxiosRequestConfig): void {
+    const headers = requestConfig.headers;
+    headers.testlinkUrl = this.parseRestURL(
+      headers.testlinkUrl,
+      headers.testlinkPort
+    );
+  }
+
+  private static parseRestURL(ip: string, port: number): string {
+    return `http://${ip}:${port}/lib/api/rest/v2`;
+  }
+
   private static parseRequestResponse<T>(
     response: AxiosRequestConfig
   ): Promise<T[]> {
     const data = response.data;
-    return data.item || data.items;
+    return data.item || data.items || data;
   }
 }
