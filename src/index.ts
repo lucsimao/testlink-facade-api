aaed8a487851a1b1f115eaba0c8a868204a3a324
lucsimao <lucasimao64@gmail.com>
2021-08-17 19:14:04 -0300

    chore: adding default config to environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
 import './util/module-alias';
+
+import { SetupServer } from './server';
+import config from 'config';
+
+(async (): Promise<void> => {
+  try {
+    const server = new SetupServer(config.get('App.port'));
+    await server.init();
+    server.start();
+  } catch (error) {
+    process.exit(1);
+  }
+})();
