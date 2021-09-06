8a52520633fa50527912b8cf19218edd5f7250af
lucsimao <lucasimao64@gmail.com>
2021-09-06 09:19:44 -0300

    feat: adding tests for fb8d7a6

--- src/client/__tests__/__integration__/testPlan.test.ts
+++ src/client/__tests__/__integration__/testPlan.test.ts
@@ -10,4 +10,20 @@ describe('TestPlan Test', () => {
     const response = await testlinkClient.getTestPlans(requestConfig, 1);
     expect(response).toEqual(testPlanFixture);
   });
+
+  it('should return something when receive a invalid id', async () => {
+    const testlinkClient = new TestlinkClient();
+    const requestConfig = TestHelper.getIntegrationTestHeader();
+    const invalidTestProjectId = 1000;
+    try {
+      await testlinkClient.getTestPlans(requestConfig, invalidTestProjectId);
+      throw new Error('Test Failed');
+    } catch (error) {
+      expect(error).toEqual(
+        new Error(
+          '[7000] (getProjectTestPlans) - The Test Project ID (1000) provided does not exist!'
+        )
+      );
+    }
+  });
 });
